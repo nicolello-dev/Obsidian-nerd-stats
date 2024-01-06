13e99f2478bd49811a61387834ed86af6f137c47
nicolello-dev <nicolamigone179@gmail.com>
2024-01-06 21:11:12 +0100

    First working version

--- main.ts
+++ main.ts
@@ -1,109 +1,107 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { cpu, mem, drive } from "node-os-utils";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface Settings extends Record<string, boolean | number> {
+	CPUView: boolean;
+	MemUsedView: boolean;
+	MemfreeView: boolean;
+	RefreshRate: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+type SettingsTabSetting = {
+	name: string;
+	desc: string;
+	setting: keyof Settings;
+	type: "toggle" | "number";
+};
+
+const DEFAULT_SETTINGS: Settings = {
+	CPUView: true,
+	MemUsedView: true,
+	MemfreeView: false,
+	RefreshRate: 1000,
+};
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: Settings;
+	statusBarElId = "obsidian-nerd-stats-status-bar-item";
+
+	async updateInfo() {
+		const statusBarEl = document.getElementById(this.statusBarElId);
+		if (statusBarEl) {
+			let displayText = "";
+			if (this.settings.CPUView) {
+				const cpuUsage = await cpu.usage();
+				displayText += `CPU: ${cpuUsage}%`;
+			}
+			if (this.settings.MemUsedView) {
+				const memUsage = await mem.info();
+				displayText += ` Memory: ${
+					100 - memUsage.freeMemPercentage
+				}% used`;
+			}
+			if (this.settings.MemfreeView) {
+				const memUsage = await mem.info();
+				const res = `${memUsage.freeMemMb}Mb free`;
+				// If memUsed is true, add parentheses. If not, add "Memory: " to the beginning
+				displayText += this.settings.MemUsedView
+					? ` (${res})`
+					: ` Memory: ${res}`;
+			}
+			statusBarEl.setText(displayText);
+		}
+	}
+
+	updateMinWidth() {
+		const statusBarItemEl = document.getElementById(this.statusBarElId);
+		if (!statusBarItemEl) return;
+		statusBarItemEl.style.minWidth =
+			[
+				this.settings.CPUView,
+				this.settings.MemUsedView,
+				this.settings.MemfreeView,
+			].filter((x) => x == true).length *
+				100 +
+			"px";
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Loading...");
+		statusBarItemEl.id = this.statusBarElId;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		this.updateInfo();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.registerInterval(
+			window.setInterval(
+				async () => await this.updateInfo(),
+				// Math.max(this.settings.RefreshRate, 250) // Minimum refresh rate of 250ms
+				1000
+			)
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		await this.updateInfo();
 	}
 }
 
@@ -116,19 +114,87 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		const settings: SettingsTabSetting[] = [
+			{
+				name: "CPU Usage",
+				desc: "Adds the CPU usage to the status bar",
+				setting: "CPUView",
+				type: "toggle",
+			},
+			{
+				name: "Memory Used Percentage",
+				desc: "Adds the memory used percentage to the status bar",
+				setting: "MemUsedView",
+				type: "toggle",
+			},
+			{
+				name: "Memory Free",
+				desc: "Adds the memory free to the status bar",
+				setting: "MemfreeView",
+				type: "toggle",
+			},
+			{
+				name: "Refresh Time (ms)",
+				desc: "How often should the plugin update the status bar in milliseconds. Default: 250",
+				setting: "RefreshRate",
+				type: "number",
+			},
+		];
+
+		for (const setting of settings) {
+			const s = new Setting(containerEl)
+				.setName(setting.name)
+				.setDesc(setting.desc);
+			if (setting.type === "toggle") {
+				s.addToggle((value) =>
+					value
+						// TS is mad because there's both number and boolean settings
+						.setValue(
+							this.plugin.settings[
+								setting.setting as keyof Settings
+							] as boolean
+						)
+						.onChange(async (value) => {
+							(this.plugin.settings[
+								setting.setting as keyof Settings
+							] as boolean) = value;
+							await this.plugin.saveSettings();
+							this.plugin.updateMinWidth();
+						})
+				);
+			} else {
+				s.addText((text) =>
+					text
+						.setValue(
+							JSON.stringify(
+								this.plugin.settings[
+									setting.setting as keyof Settings
+								]
+							)
+						)
+						.onChange(async (value) => {
+							try {
+								parseInt(value);
+							} catch (e) {
+								console.error(e);
+								console.error(
+									"Couldn't parse value, got",
+									value,
+									typeof value
+								);
+								return;
+							}
+							(this.plugin.settings[
+								setting.setting as keyof Settings
+							] as number) = parseInt(value);
+							await this.plugin.saveSettings();
+						})
+				);
+			}
+		}
 	}
 }
